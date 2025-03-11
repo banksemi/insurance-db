36dd9fff957f3979c91bc09137f689346ca1d602
Seunghwa Lee <banksemi@naver.com>
2025-03-11 20:55:02 +0900

    프로젝트 생성

---
+++ caregiver-management/src/main/java/com/sideproject/caregiver_management/CaregiverManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.caregiver_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregiverManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaregiverManagementApplication.class, args);
+	}
+
+}
---
+++ caregiver-management/src/test/java/com/sideproject/caregiver_management/CaregiverManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.caregiver_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregiverManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
