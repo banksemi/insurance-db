4aab6ccc056b741d47b9f59e58597104fae9f7e7
Seunghwa Lee <banksemi@naver.com>
2025-03-13 21:21:29 +0900

    test: 테스트 환경에서 H2 인메모리 DB를 사용하도록 수정

--- caregiver-management/src/main/java/com/sideproject/caregiver_management/user/entity/User.java
+++ caregiver-management/src/main/java/com/sideproject/caregiver_management/user/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "users")  // 테스트시 사용하는 H2 데이터베이스에서 "user"는 예약어이므로 다른 이름을 사용해야함.
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 외부에서 무분별한 new User()을 막자
 // @Builder 을 쓰면 @AllArgsConstructor 을 써야하는데 필드 순서에 의존하므로 위험하다.
 // 따라서 수동으로 필요한 항목만 직접 지정해서 순서가 의도치 않게 바뀌는 문제를 해결해보자.
