3dc6c5cf5e770d061798e5e384fa8bd1db3e32de
Seunghwa Lee <banksemi@naver.com>
2025-03-23 21:35:50 +0900

    fix: User 엔티티가 Converter로 인해 더티 체킹이 일어나지 않도록 오류 수정

--- caregiver-management/src/main/java/com/sideproject/caregiver_management/auth/dto/EncodedPassword.java
+++ caregiver-management/src/main/java/com/sideproject/caregiver_management/auth/dto/EncodedPassword.java
@@ -3,9 +3,19 @@ package com.sideproject.caregiver_management.auth.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @RequiredArgsConstructor
 public class EncodedPassword {
     private final String prefix;
     private final String value;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EncodedPassword that = (EncodedPassword) o;
+        return Objects.equals(prefix, that.prefix) && Objects.equals(value, that.value);
+    }
 }
